80d1d35e15a0e79435b2cd2598bd698ae05da33b
Raffly Putra Ramadan <rafflyputrar@gmail.com>
2025-03-21 13:14:51 +0700

    Merapikan kode koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -1,15 +1,17 @@
 <?php
 
-$host = 'localhost';  
-$user = 'root';
-$password = '';   
+// Konfigurasi database
+$host     = 'localhost';
+$user     = 'root';
+$password = '';
 $database = 'time_management';
 
+// Membuat koneksi ke database
 $koneksi = mysqli_connect($host, $user, $password, $database);
 
+// Periksa koneksi
 if (!$koneksi) {
     die("Koneksi gagal: " . mysqli_connect_error());
 }
 
 ?>
-
